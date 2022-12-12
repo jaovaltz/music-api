89f7dea2bbd247d8245a6615bd17a8ca834eadf4
wellapos <wellington258@live.com>
2022-12-11 21:37:34 -0300

    redis

--- src/controllers/music.controller.ts
+++ src/controllers/music.controller.ts
@@ -1,43 +1,78 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
-import { Music } from "../models/music.model";
+import { Music } from '../models/music.model'
+import { getRedis, setRedis } from '../redisConfig'
 
 const createMusic = async (req: Request, res: Response) => {
-  const { title, artist, album, genre, year } = req.body;
+  const { title, artist, album, genre, year } = req.body
 
-  const music = Music.create({
+  const music = await Music.create({
     title,
     artist,
     album,
     genre,
-    year,
-  });
+    year
+  })
 
   if (music) {
-    return res.status(201).json(music);
+    return res.status(201).json(music)
   } else {
-    return res.status(400).json({ error: "Something went wrong" });
+    return res.status(400).json({ error: 'Something went wrong' })
   }
-};
+}
 
 const getMusics = async (req: Request, res: Response) => {
-  const musics = await Music.find({});
-  return res.status(200).json(musics);
-};
+  const cachedKey = 'musics:all'
+
+  console.time('All Musics')
+
+  const cachedMusics = await getRedis(cachedKey)
+
+  if (cachedMusics) {
+    console.log('cached')
+    const parsedMusics = JSON.parse(cachedMusics)
+    console.timeEnd('All Musics')
+    return res.status(200).json(parsedMusics)
+  }
+
+  const musics = await Music.find({})
+
+  console.timeEnd('All Musics')
+
+  await setRedis(cachedKey, JSON.stringify(musics))
+
+  return res.status(200).json(musics)
+}
 
 const getMusicById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const music = await Music.findById(id);
-  if (music) {
-    return res.status(200).json(music);
-  } else {
-    return res.status(404).json({ error: "Music not found" });
+  const { id } = req.params
+
+  const cachedKey = `music:${id}`
+
+  console.time('music')
+
+  const musicRedis = await getRedis(cachedKey)
+
+  if (musicRedis) {
+    const parsedMusic = JSON.parse(musicRedis)
+    console.timeEnd('music')
+    return res.status(200).json(parsedMusic)
   }
-};
+
+  const music = await Music.findById(id)
+
+  if (music) {
+    await setRedis(cachedKey, JSON.stringify(music))
+    console.timeEnd('music')
+    return res.status(200).json(music)
+  }
+
+  return res.status(404).json({ error: 'Music not found' })
+}
 
 const updateMusic = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { title, artist, album, genre, year } = req.body;
+  const { id } = req.params
+  const { title, artist, album, genre, year } = req.body
   const music = await Music.findByIdAndUpdate(
     id,
     {
@@ -45,25 +80,25 @@ const updateMusic = async (req: Request, res: Response) => {
       artist,
       album,
       genre,
-      year,
+      year
     },
     { new: true }
-  );
+  )
   if (music) {
-    return res.status(200).json(music);
+    return res.status(200).json(music)
   } else {
-    return res.status(404).json({ error: "Music not found" });
+    return res.status(404).json({ error: 'Music not found' })
   }
-};
+}
 
 const deleteMusic = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const music = await Music.findByIdAndDelete(id);
+  const { id } = req.params
+  const music = await Music.findByIdAndDelete(id)
   if (music) {
-    return res.status(200).json(music);
+    return res.status(200).json(music)
   } else {
-    return res.status(404).json({ error: "Music not found" });
+    return res.status(404).json({ error: 'Music not found' })
   }
-};
+}
 
-export { createMusic, getMusics, getMusicById, updateMusic, deleteMusic };
+export { createMusic, getMusics, getMusicById, updateMusic, deleteMusic }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express'
+import mongoose from 'mongoose'
 
-import musicRouter from "./routes/music.routes";
+import musicRouter from './routes/music.routes'
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017/music').then(() => {
+  const app = express()
 
-app.use(express.json());
-app.use("/musics", musicRouter);
+  app.use(express.json())
+  app.use('/musics', musicRouter)
 
-mongoose.connect("mongodb://localhost:27017/music", {});
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
+  app.listen(3001, () => {
+    console.log('Server is running on port 3001')
+  })
+})
